0f6c359da5e21bd53c106749573186af29c15edd
Nguyen Le Nguyen <nglenguyenn@gmail.com>
2023-10-31 15:24:26 +0700

    Revert "add admin-react"
    This reverts commit c1e2b4f0e285721e0b1019d0627f207718adf42a.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Viettel_Solution.Models;
 using Viettel_Solution.Storage;
@@ -12,10 +11,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Register AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "client/admin-react";
-});
 
 //Add services for Sql Server
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -35,20 +30,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.UseSpaStaticFiles();
+
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "client/admin-react";
-
-    if (app.Environment.IsDevelopment())
-    {
-        spa.UseReactDevelopmentServer(npmScript: "start");
-    }
-});
 
 app.Run();
